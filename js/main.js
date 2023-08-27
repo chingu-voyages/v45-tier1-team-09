a840a7dcc3d6d846d7371693db8509ae7f9f5e34
xITSDUCKYx <82958945+xITSDUCKYx@users.noreply.github.com>
2023-08-27 13:10:41 -0400

    add basic plot point grab

--- js/main.js
+++ js/main.js
@@ -36,5 +36,74 @@ clearBtn.addEventListener('click', () => {
 })
 
 
+
+fetch("assets/Meteorite_Landings.csv")
+.then(response => response.text())
+.then(csvData => {
+  const lines = csvData.split("\n"); // Split data into lines
+  const headers = lines[0].split(","); // Extract headers
+  
+  const data = [];
+  for (let i = 1; i < lines.length; i++) {
+    const values = lines[i].split(",");
+    if (values.length === headers.length) {
+      const entry = {};
+      for (let j = 0; j < headers.length; j++) {
+        entry[headers[j]] = values[j];
+      }
+      data.push(entry);
+    }
+  }
+  
+  histogramPlotPoints(data);
+})
+.catch(error => {
+  console.error("Error fetching CSV data:", error);
+});
+
+
+
+function histogramPlotPoints(data) {
+  const yearCount = {}
+  const compCount = {}
+
+  data.forEach(element => {
+
+    const year = element.year;
+      if (!yearCount[year]) {
+        yearCount[year] = 1
+      } else {
+        yearCount[year]++
+      }
+
+    const composition = element.recclass
+      if (!compCount[composition]) {
+        compCount[composition] = 1
+      } else {
+        compCount[composition]++
+      }
+});
+
+  const yearPlotPoints = [];
+  for (const year in yearCount) {
+    if (yearCount.hasOwnProperty(year)) {
+      yearPlotPoints.push({ year: year, count: yearCount[year] });
+    }
+  }
+
+  const compositionPlotPoints = []
+  for (const composition in compCount) {
+    if (compCount.hasOwnProperty(composition)){
+      compositionPlotPoints.push({composition: composition, count: compCount[composition]})
+    }
+  }
+
+  
+  
+console.log(yearPlotPoints)
+console.log(compositionPlotPoints)
+}
+
+
 //Input Validation and Error Message
 
