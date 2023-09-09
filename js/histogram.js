62c33118f7292f8fef0634358a1765a051362a83
xITSDUCKYx <82958945+xITSDUCKYx@users.noreply.github.com>
2023-09-09 14:17:16 -0400

    Histogram tweaks

--- js/histogram.js
+++ js/histogram.js
@@ -35,7 +35,7 @@
 
 
 // function histogramPlotPoints(data) {
-//   const yearCount = {}
+//   const selectedValueCount = {}
 //   const compCount = {}
 
 //   data.forEach(element => {
@@ -78,44 +78,88 @@
 
 // }
 
+
+
   
-    let chartWidth = window.screen.availWidth - 150;
+    let chartWidth = window.innerWidth;
 
     window.addEventListener("resize", function(){
-      if (chartWidth > 600){
-        chartWidth = 600
-       return chartWidth
-    } else if (chartwidth < 1000) {
-      chartWidth = 950
-    } else {
-      console.log(chartWidth)
-      return chartWidth
-    }
+      
+      chartWidth < 600 ? chartWidth = 600  
+     : chartWidth > 1000 ?  chartWidth = 1000
+     : chartWidth;
+      
     })
-    
-    
 
-   
+
 	// set the dimensions and margins of the graph
   var margin = {top: 10, right: 30, bottom: 30, left: 40},
-  width = 900 - margin.left - margin.right,
+  width = chartWidth - margin.left - margin.right,
   height = width - 60 - margin.top - margin.bottom;
-  
-  
-  console.log(chartWidth)
+
+ 
+
+
+
 
 // append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
+var svg = d3.select("#yearChart")
   .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
+      "translate(" + margin.left + "," + margin.top + ")")
+ 
 
 // get the data
-// d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", function(data) {
+
+
+
+
+
+
+
+// function updateChart(data) {
+
 d3.csv("assets/Meteorite_Landings.csv", function(data) {
+    
+// d3.json("https://data.nasa.gov/resource/gh4g-9sfh.json", function(data) {
+
+// function updateChart(selectedValue) {
+//   // Filter the data based on the selected value
+//   var filteredData = data.filter(function (d) {
+//       if (selectedValue === "year") {
+        
+//           return d.year
+//       } else if (selectedValue === "reclass") {
+          
+//           return d.reclass
+//       } 
+      
+//       // Return true to include all data when no filter is selected
+//       return true;
+//   });
+
+//   // Update your chart with the filtered data
+//   // You will need to adjust your charting logic here
+
+//   console.log(filteredData); // For testing purposes, log the filtered data
+//   console.log(selectedValue)
+// }
+
+// // Add an event listener to the select element
+// var select = d3.select("#data-type");
+// select.on("change", function () {
+//   var selectedValue = select.property("value");
+//   updateChart(selectedValue); // Call the updateChart function with the selected value
+// });
+
+// // Initially, load the chart with all data
+// updateChart();
+
+
+
 
 
   // X axis: scale and draw:
@@ -129,9 +173,9 @@ d3.csv("assets/Meteorite_Landings.csv", function(data) {
 
   // set the parameters for the histogram
   var histogram = d3.histogram()
-    .value(function(d) { return d.year; })   // I need to give the vector of value
+    .value(function(d) { return d.year})   // I need to give the vector of value
     .domain(x.domain())  // then the domain of the graphic
-    .thresholds(x.ticks(25)); // then the numbers of bins
+    .thresholds(x.ticks(70)); // then the numbers of bins
 
   // And apply this function to data to get the bins
   // var bins = histogram(data);
@@ -143,16 +187,22 @@ d3.csv("assets/Meteorite_Landings.csv", function(data) {
     y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
   svg.append("g")
     .call(d3.axisLeft(y));
+    
 
   // append the bar rectangles to the svg element
   svg.selectAll("rect")
-    .data(bins)
-    .enter()
-    .append("rect")
-    .attr("x", 1)
-    .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-    .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-    .attr("height", function(d) { return height - y(d.length); })
-    .style("fill", "#69b3a2")
-
+  .data(bins)
+  .enter()
+  .append("rect")
+  .attr("x", 1)
+  .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+  .attr("width", function(d) { return x(d.x1) - x(d.x0) - 1; })
+  .attr("height", function(d) { return height - y(d.length); })
+  .style("fill", "#008080")
+  .append("title")
+  .text((d)=> {
+    return "Year" + ": " + d.x0 + " - " + d.x1 
+  })
+ 
+   
 });
