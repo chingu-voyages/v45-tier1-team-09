5c00e8f0e3261dafb3bf994e9e66cd51c49d9064
Alex Wu <8242698+wu-coding@users.noreply.github.com>
2023-09-20 06:54:58 -0400

    release

---
+++ js/saveHistory.js
@@ -0,0 +1,11 @@
+//const searchData = searchForm.querySelectorAll("input")
+
+export function fillForm(searchData){
+    //Recieve array fill it
+        
+}
+
+//Sort
+function sort(array){
+var pivot = array.length/2 
+}
